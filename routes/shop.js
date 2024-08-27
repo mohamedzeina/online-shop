52360be0d0224986f6109e07264f4a58c7ffae49
Mohamed Zeina <toxiczeina@gmail.com>
2024-08-27 13:37:55 +0300

    served the html pages created

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // __dirname holds path of project folder of this current file
 });
 
 module.exports = router;
