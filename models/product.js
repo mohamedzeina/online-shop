71e1b0ed89d43e176065e6727a0f6c83f420afac
Mohamed Zeina <toxiczeina@gmail.com>
2024-08-29 14:27:21 +0300

    stored products in a file via model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
   constructor(title) {
@@ -6,11 +7,37 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, (err, fileContnet) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContnet);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
     // Static to call the fucntion directly without an instance
-    return products;
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
