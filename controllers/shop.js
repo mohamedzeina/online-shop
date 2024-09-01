6c7d728b9fb28d0d4015072c19f5f18a291ab0ef
Mohamed Zeina <toxiczeina@gmail.com>
2024-09-01 16:33:07 +0300

    fetching products from db instead of local file for products page

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products',
-    }); // Passing options to the template
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products',
+      }); // Passing options to the template
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
