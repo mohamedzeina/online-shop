763a0bb02322b580ec73f6d9c139f75c3000f18c
Mohamed Zeina <toxiczeina@gmail.com>
2024-09-02 21:47:48 +0300

    resetting cart after user places order and fetching and outputting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -123,13 +123,26 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', { path: '/orders', pageTitle: 'Your Orders' });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -147,14 +160,13 @@ exports.postOrder = (req, res, next) => {
           console.log(err);
         });
     })
-    .then((result) => {
+    .then(() => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch((err) => {
       console.log(err);
     });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', { path: '/checkout', pageTitle: 'Checkout' });
 };
