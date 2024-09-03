3db556ed3bb641c8e5fefcc75c7772e8d78aaa23
Mohamed Zeina <toxiczeina@gmail.com>
2024-09-03 16:06:03 +0300

    updating products now works

--- models/product.js
+++ models/product.js
@@ -2,18 +2,27 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the product if _id is set
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      // Insert new product if _id is not set
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then((res) => {
         console.log(res);
       })
