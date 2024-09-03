9a5dfd705267c9d7e3a68b8381bfc2770b408e7e
Mohamed Zeina <toxiczeina@gmail.com>
2024-09-03 23:17:20 +0300

    added relational order data when placing an order

--- models/user.js
+++ models/user.js
@@ -80,9 +80,19 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+
+        return db.collection('orders').insertOne(order);
+      })
       .then(() => {
         this.cart = { items: [] };
         return db.collection('users').updateOne(
@@ -93,6 +103,11 @@ class User {
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db.collection('orders').find;
   }
 
   static findById(userId) {
