8c3b29c37f5b6af60e5befd9c35e6cc69b3cdf9f
Mohamed Zeina <toxiczeina@gmail.com>
2024-09-03 16:31:05 +0300

    refactored user model and added a dummy user

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,16 +18,16 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false })); // Parses body like we used to do manually in previous http version of this project
 app.use(express.static(path.join(__dirname, 'public'))); // Grant read access to the public folder statically
 
-// app.use((req, res, next) => {
-//   User.findByPk(1)
-//     .then((user) => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// }); // Storing dummy user to be able to use it anywhere in the app
+app.use((req, res, next) => {
+  User.findById('66d70f0e666e1fffe2d5dd9c')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}); // Storing dummy user to be able to use it anywhere in the app
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
